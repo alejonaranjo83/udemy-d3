e8e27c25b6014989a4a8f4d9452df214e730ada5
alejonaranjo83-linux <alejonaranjo83@gmail.com>
2023-11-07 19:11:48 -0500

    Playing with SVGs shapes

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -4,10 +4,12 @@
 *    2.4 - Adding SVGs with D3
 */
 
+
+// Adding SVGs using the "Method chaining"
+
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
   .attr("height", 400)
-
 
 svg.append("rect")
   .attr("x", 10)
