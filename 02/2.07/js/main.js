6d56a6083031e5f75b26d57ff021a0f41ea0a7c5
alejonaranjo83-linux <alejonaranjo83@gmail.com>
2023-11-11 09:42:21 -0500

    commenting and understanding the exercise

--- 02/2.07/js/main.js
+++ 02/2.07/js/main.js
@@ -11,29 +11,29 @@
 
 d3.json("/2.07/data/ages.json").then(data => {
 	console.log(data)
-	data.forEach(d => {
+	data.forEach(d => { //converting age format to "Number"
 		d.age = Number(d.age)
 	})
 	
-	const svg = d3.select("#chart-area").append("svg")
-	.attr("width", 400)
-	.attr("height", 400)
+	const svg = d3.select("#chart-area").append("svg") //creating canvas for locating shapes that are going to be created
+		.attr("width", 400)
+		.attr("height", 400)
 
 	const circles = svg.selectAll("circle")
 		.data(data)
 
 	circles.enter().append("circle")
-		.attr("cx", (d, i) => (i * 50) + 50)
-		.attr("cy", 250)
-		.attr("r", (d) => 2 * d.age)
+		.attr("cx", (d, i) => (i * 50) + 50) //defining position on X axis
+		.attr("cy", 250) //defining position on Y axis
+		.attr("r", (d) => 2 * d.age) //radius based on age value times 2
 		.attr("fill", d => {
-			if (d.name === "Tony") {
+			if (d.name === "Emily") { //assigning colors based on names
 				return "blue"
 			}
 			else {
 				return "red"
 			}
 		})
-}).catch(error => {
+}).catch(error => { //if there's an error, show it on console
 	console.log(error)
 })
