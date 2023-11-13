8f3111bb3384f3c1920777993626be8fd85835fb
alejonaranjo83-linux <alejonaranjo83@gmail.com>
2023-11-13 10:44:52 -0500

    Scaling the height of the elements + parameterizing that height

--- 03/3.02.b/js/main.js
+++ 03/3.02.b/js/main.js
@@ -4,18 +4,23 @@
 *    3.2 - Linear scales
 */
 
+// Personal attempt to parametrize the height as a function of the total document height
+
+// 1. Create a constant to define the maximum height
+const dh = ($(document).height())/2
+
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
-  .attr("height", 400)
+  .attr("height", dh) //2. using the const to define the height of the SVG canvas
 
 d3.json("data/buildings.json").then(data => {
   data.forEach(d => {
     d.height = Number(d.height)
   })
 
-  const y = d3.scaleLinear() //Creating a constant to scale values
+  const y = d3.scaleLinear() //3. Creating a constant to scale values
     .domain([0, 828]) //the limits of real data
-    .range([0, 400]) //limits proportional to the space set in D3
+    .range([0, dh]) //limits proportional to the space set in D3
 
   const rects = svg.selectAll("rect")
     .data(data)
